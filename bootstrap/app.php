3c0538fcc2ad2b9bf810d8963ecebee6630bc4d4
nas11ai <kairupannasai@gmail.com>
2026-01-08 13:26:03 +0800

    refactor: remove unused import

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Throwable;
 
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
